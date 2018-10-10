8ef49a2808b05caa1e8870dbfcf38e2999b92928
yjcia <yjciacia@gmail.com>
2018-10-10 21:39:26 +0800

    concurrency

---
+++ src/main/java/com/yanjun/concurrency/concurrencydemo/ConcurrencyDemoApplication.java
@@ -0,0 +1,12 @@
+package com.yanjun.concurrency.concurrencydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrencyDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurrencyDemoApplication.class, args);
+	}
+}
---
+++ src/main/java/com/yanjun/concurrency/concurrencydemo/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.yanjun.concurrency.concurrencydemo;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ConcurrencyDemoApplication.class);
+	}
+
+}
